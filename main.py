d910cab3867ba606899b06dbd518bd9cde701b2f
pascal910107 <95022717+pascal910107@users.noreply.github.com>
2022-11-29 23:22:40 +0800

    delete unuse file

--- main.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-
-response = requests.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-A4CB2B13-29D3-4814-A575-3FBE669C55C6&format=JSON')
-print(response.text)
